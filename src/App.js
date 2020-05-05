cc9fc3ef9f25ce6b92c807c54a2086830f3f30e9
Ella Aronov <ellaarono1994@gmail.com>
2020-05-05 16:17:22 +0300

    Created a header for the site

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PrimarySearchAppBar from './components/primarySearchAppBar/PrimarySearchAppBar'
+
+function App() {
+  return (
+    <div >
+      <PrimarySearchAppBar />
+    </div>
+  );
+}
+
+export default App;
