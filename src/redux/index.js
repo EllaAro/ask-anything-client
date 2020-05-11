1228a64282aa21b84e6e4926841a5595d24dba6a
Ella Aronov <ellaarono1994@gmail.com>
2020-05-11 23:48:42 +0300

    Implemented graphql usage and redux-thunk

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,11 +1,13 @@
-import redux, {createStore, combineReducers, applyMiddleware} from 'redux'
-import thunk from 'redux-thunk'
+import {createStore, combineReducers, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
 import privateSettingsReducer from './privateSettings';
+import postsReducer from './posts';
 
 
 
 const rootReducer = combineReducers({
     privateSettings: privateSettingsReducer,
+    posts: postsReducer,
 })
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
