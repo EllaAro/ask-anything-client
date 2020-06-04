9ffa254c0febbf3f15f1ff7cebb0eb4e0a8a470d
Ella Aronov <ellaarono1994@gmail.com>
2020-06-04 14:40:56 +0300

    Addded makeStyles functionality for the Comment component

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,10 +1,18 @@
 import React from "react";
 import { Avatar, Grid } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  name: { margin: 0, textAlign: "left" },
+  content: { textAlign: "left" },
+  date: { textAlign: "left", color: "gray" },
+});
 
 const imgLink =
   "https://images.pexels.com/photos/1681010/pexels-photo-1681010.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260";
 
 const Comment = ({ userInfo }) => {
+  const classes = useStyles();
   const { content, createdAt, firstName, lastName } = userInfo;
   return (
     <Grid container wrap="nowrap" spacing={2}>
@@ -12,13 +20,9 @@ const Comment = ({ userInfo }) => {
         <Avatar alt="Remy Sharp" src={imgLink} />
       </Grid>
       <Grid justifyContent="left" item xs zeroMinWidth>
-        <h4
-          style={{ margin: 0, textAlign: "left" }}
-        >{`${firstName} ${lastName}`}</h4>
-        <p style={{ textAlign: "left" }}>{content} </p>
-        <p style={{ textAlign: "left", color: "gray" }}>
-          posted at ${createdAt}
-        </p>
+        <h4 className={classes.name}>{`${firstName} ${lastName}`}</h4>
+        <p className={classes.content}>{content} </p>
+        <p className={classes.date}>posted at ${createdAt}</p>
       </Grid>
     </Grid>
   );
