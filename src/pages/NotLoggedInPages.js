5e0c2f85dd79e2e5728dbe421a1f7f23b7c84ff0
Ella Aronov <ellaarono1994@gmail.com>
2020-07-01 10:59:39 +0300

    Working on the SignIn and SignUp pages

--- src/pages/NotLoggedInPages.js
+++ src/pages/NotLoggedInPages.js
@@ -2,39 +2,6 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
-import ParticlesBg from "particles-bg";
-
-let config = {
-  num: [4, 7],
-  rps: 0.1,
-  radius: [5, 40],
-  life: [1.5, 3],
-  v: [2, 3],
-  tha: [-40, 40],
-  alpha: [0.6, 0],
-  scale: [0.1, 0.4],
-  position: "all",
-  color: ["random", "#ff0000"],
-  cross: "dead",
-  random: 15,
-};
-
-if (Math.random() > 0.85) {
-  config = Object.assign(config, {
-    onParticleUpdate: (ctx, particle) => {
-      ctx.beginPath();
-      ctx.rect(
-        particle.p.x,
-        particle.p.y,
-        particle.radius * 2,
-        particle.radius * 2
-      );
-      ctx.fillStyle = particle.color;
-      ctx.fill();
-      ctx.closePath();
-    },
-  });
-}
 
 const NotLoggedInPages = () => (
   <React.Fragment>
@@ -47,7 +14,6 @@ const NotLoggedInPages = () => (
       </Route>
       <Route render={() => <SignIn />} />
     </Switch>
-    <ParticlesBg type="custom" config={config} bg={true} />
   </React.Fragment>
 );
 
