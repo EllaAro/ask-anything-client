6c5b605a917204c3a513323443e65cf778d20a9b
Ella Aronov <ellaarono1994@gmail.com>
2020-07-10 13:36:11 +0300

    Added more switch case options for the posts reducer as well as new action types

--- src/pages/RecommendedUserPostsPage.js
+++ src/pages/RecommendedUserPostsPage.js
@@ -1,27 +1,3 @@
-// import React, { useEffect } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { fetchRecommendedPosts } from "../redux/actions/postsActions";
-// import Post from "../components/sharedComponents/Post";
-
-// const RecommendedUserPostsPage = () => {
-//   const dispatch = useDispatch();
-//   const { token } = useSelector((state) => state.auth);
-
-//   useEffect(() => {
-//     dispatch(fetchRecommendedPosts(token));
-//   }, []);
-
-//   const { posts, loading, fetched, error } = useSelector(
-//     (state) => state.posts.recommendedPosts
-//   );
-//   const resPosts =
-//     posts.length > 0 ? posts.map((post) => <Post postData={post} />) : <div />;
-
-//   return <div style={{ margin: "5em" }}>{resPosts}</div>;
-// };
-
-// export default RecommendedUserPostsPage;
-
 import React from "react";
 import { fetchRecommendedPosts } from "../redux/actions/postsActions";
 import { Posts } from "../components/sharedComponents/Posts";
--- src/pages/TrendingPostsPage.js
+++ src/pages/TrendingPostsPage.js
@@ -3,7 +3,6 @@ import { fetchTrendingPosts } from "../redux/actions/postsActions";
 import { Posts } from "../components/sharedComponents/Posts";
 
 const TrendingPostsPage = () => {
-  // return <div></div>;
   return <Posts action={fetchTrendingPosts} postsType="trendingPosts" />;
 };
 
